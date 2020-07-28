60702d0253cf81ff784b674c3b69d1fcdc2ffa18
donat <donat.baier@gmail.com>
2020-07-28 23:51:28 +0200

    block is responding

--- blocks/partners/view.php
+++ blocks/partners/view.php
@@ -9,11 +9,14 @@
   <?php foreach ($sub_page_ids as $page_id) {
     $page = \Page::getByID($page_id, 'ACTIVE');
 
-    echo $page->getCollectionName();
-    echo Loader::helper('navigation')->getLinkToCollection($page);
+    $link_target  = Loader::helper('navigation')->getLinkToCollection($page);
+    $link_text    = $page->getCollectionName();
+    $color        = sprintf('#%06X', mt_rand(0, 0xFFFFFF));
 
     if (($info_block_id = array_search('partner_info', array_column($page->getBlocks(), 'btHandle'))) !== false) {
-      // $info_block = $page->getBlocks()[$info_block_id];
+      $color = $page->getBlocks()[$info_block_id]->getInstance()->color;
     }
+
+    echo "<a href='$link_target' class='button btn btn-lg' style='background: $color; color: black'>$link_text</a>";
   } ?>
 </div>
