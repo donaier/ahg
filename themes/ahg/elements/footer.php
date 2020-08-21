64d22d6aa967e10447da0b35fe7fa9cb76e14f6a
donat <donat.baier@gmail.com>
2020-08-21 09:03:45 +0200

    ufrum

--- themes/ahg/elements/footer.php
+++ themes/ahg/elements/footer.php
@@ -1,40 +1,25 @@
-<?php defined('C5_EXECUTE') or die("Access Denied.");
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
 
-$footerSiteTitle = new GlobalArea('Footer Site Title');
-$footerSiteTitleBlocks = $footerSiteTitle->getTotalBlocksInArea();
-
-$footerSocial = new GlobalArea('Footer Social');
-$footerSocialBlocks = $footerSocial->getTotalBlocksInArea();
-
-$displayFirstSection = $footerSiteTitleBlocks > 0 || $footerSocialBlocks > 0 || $c->isEditMode();
-?>
-
-<footer id="footer-theme">
-    <?php
-    if ($displayFirstSection) {
-        ?>
-        <section>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-9">
-                        <?php
-                        $a = new GlobalArea('Footer Contact');
-                        $a->display();
-                        ?>
-                    </div>
-                    <div class="col-sm-3">
-                        <?php
-                        $a = new GlobalArea('Footer Social');
-                        $a->display();
-                        ?>
-                        <?php echo Core::make('helper/navigation')->getLogInOutLink() ?>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <?php
-    }
-    ?>
+<footer>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-9">
+          <?php
+          // $a = new GlobalArea('Footer Contact');
+          // $a->display();
+          ?>
+        </div>
+        <div class="col-sm-3">
+          <?php
+          // $a = new GlobalArea('Footer Social');
+          // $a->display();
+          ?>
+          <?php echo Core::make('helper/navigation')->getLogInOutLink() ?>
+        </div>
+      </div>
+    </div>
+  </section>
 </footer>
 
 
--- themes/ahg/elements/header.php
+++ themes/ahg/elements/header.php
@@ -5,20 +5,20 @@ $this->inc('elements/header_top.php');
 ?>
 
 <header>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4 col-xs-6">
-                <?php
-                $a = new GlobalArea('Header Site Title');
-                $a->display();
-                ?>
-            </div>
-            <div class="col-sm-8 col-xs-6">
-                <?php
-                $a = new GlobalArea('Header Navigation');
-                $a->display();
-                ?>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4 col-xs-6">
+        <?php
+        // $a = new GlobalArea('Header Site Title');
+        // $a->display();
+        ?>
+      </div>
+      <div class="col-sm-8 col-xs-6">
+        <?php
+        // $a = new GlobalArea('Header Navigation');
+        // $a->display();
+        ?>
+      </div>
     </div>
+  </div>
 </header>
