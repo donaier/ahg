f38b5f11ac702db9220a7b99fdcbf4ded7716734
donat <donat.baier@gmail.com>
2020-08-22 23:45:26 +0200

    w filter

--- blocks/partners/form.php
+++ blocks/partners/form.php
@@ -1,20 +1,25 @@
-<?php
-
-defined('C5_EXECUTE') or die(_("Access Denied."));
-
-?>
-
+<?php defined('C5_EXECUTE') or die(_("Access Denied.")); ?>
 
 <div class="form-group">
   <label class="control-label" for="top_site">Unter welcher Seite sind die Partner/Geschäfte?</label>
-
   <select name="top_site">
-    <?php 
-    foreach ($pages as $page) {
-    ?>
+    <?php foreach ($pages as $page) { ?>
       <option value="<?=$page->cID?>" <?= $page->cID == $top_site ? 'selected' : ''?>><?=$page->getCollectionName()?></option>
-    <?php
-    }
-    ?>
+    <?php } ?>
   </select>
+</div>
+
+<div class="form-group">
+  <label class="control-label" for="einsatzQ">Text "Einsatz"</label>
+  <input type="text" name="einsatzQ">
+</div>
+
+<div class="form-group">
+  <label class="control-label" for="einkaufQ">Text "Einkauf"</label>
+  <input type="text" name="einkaufQ">
+</div>
+
+<div class="form-group">
+  <label class="control-label" for="gemeinschaftQ">Text "Gemeinschaft"</label>
+  <input type="text" name="gemeinschaftQ">
 </div>
--- blocks/partners/view.php
+++ blocks/partners/view.php
@@ -5,35 +5,70 @@
   $sub_page_ids = $page->getCollectionChildrenArray();
 ?>
 
-<div class="container">
-  <div class="row">
-    <?php foreach ($sub_page_ids as $page_id) {
-      $page = \Page::getByID($page_id, 'ACTIVE');
+<div class="row filter-q">
+  <?php if (isset($einsatzQ)) { ?>
+    <div class="col-xs-12">
+      <button class="filter-button btn" filter-data="einsatz" onclick="applyQ('einsatz');">
+        <?= $einsatzQ ?>
+      </button>
+    </div>
+  <?php } ?>
+  <?php if (isset($einkaufQ)) { ?>
+    <div class="col-xs-12">
+      <button class="filter-button btn" filter-data="einkauf" onclick="applyQ('einkauf');">
+        <?= $einkaufQ ?>
+      </button>
+    </div>
+  <?php } ?>
+  <?php if (isset($gemeinschaftQ)) { ?>
+    <div class="col-xs-12">
+      <button class="filter-button btn" filter-data="gemeinschaft" onclick="applyQ('gemeinschaft');">
+        <?= $gemeinschaftQ ?>
+      </button>
+    </div>
+  <?php } ?>
+</div>
 
-      $link_target  = Loader::helper('navigation')->getLinkToCollection($page);
-      $link_text    = $page->getCollectionName();
-      $logo_url = '';
-      $cat_str = '';
+<div class="row">
+  <?php foreach ($sub_page_ids as $page_id) {
+    $page = \Page::getByID($page_id, 'ACTIVE');
 
-      if (($info_block_id = array_search('partner_info', array_column($page->getBlocks(), 'btHandle'))) !== false) {
-        $b = $page->getBlocks()[$info_block_id]->getInstance();
-        if ($b->logo) {
-          $logo_url = \File::getByID($b->logo)->getUrl();
-        }
-        foreach (['einsatz', 'einkauf', 'gemeinschaft'] as $cat) {
-          if ($b->$cat) {
-            $cat_str .= " $cat";
-          }
+    $link_target  = Loader::helper('navigation')->getLinkToCollection($page);
+    $link_text    = $page->getCollectionName();
+    $logo_url = '';
+    $cat_str = '';
+
+    if (($info_block_id = array_search('partner_info', array_column($page->getBlocks(), 'btHandle'))) !== false) {
+      $b = $page->getBlocks()[$info_block_id]->getInstance();
+      if ($b->logo) {
+        $logo_url = \File::getByID($b->logo)->getUrl();
+      }
+      foreach (['einsatz', 'einkauf', 'gemeinschaft'] as $cat) {
+        if ($b->$cat) {
+          $cat_str .= " $cat";
         }
       }
-    ?>
+    }
+  ?>
 
-    <div class="col-xs-4 col-md-2 partner <?= $cat_str ?>">
-      <a href="<?= $link_target ?>">
-        <img src="<?= $logo_url ?>" alt="">
-      </a>
-    </div>
-
-    <?php } ?>
+  <div class="col-xs-4 col-md-2 partner <?= $cat_str ?>">
+    <a href="<?= $link_target ?>">
+      <img src="<?= $logo_url ?>" alt="">
+    </a>
   </div>
+
+  <?php } ?>
 </div>
+
+<script>
+  function applyQ(q) {
+    console.log(q)
+    $.each($('.partner'), function( index, value ) {
+      if (!$(value).attr('class').includes(q)) {
+        $(value).hide()
+      } else {
+        $(value).show()
+      }
+    });
+  }
+</script>
